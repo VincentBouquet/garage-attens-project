158130cdd84f853820feb6c3caa3b1ea4548179f
Antoine6191 <antoine.hervieu@gmail.com>
2020-10-14 16:41:51 +0200

    creation relation

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ClientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Client
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Intervention::class, mappedBy="Client")
+     */
+    private $interventions;
+
+    public function __construct()
+    {
+        $this->interventions = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,37 @@ class Client
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Intervention[]
+     */
+    public function getInterventions(): Collection
+    {
+        return $this->interventions;
+    }
+
+    public function addIntervention(Intervention $intervention): self
+    {
+        if (!$this->interventions->contains($intervention)) {
+            $this->interventions[] = $intervention;
+            $intervention->setClient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIntervention(Intervention $intervention): self
+    {
+        if ($this->interventions->contains($intervention)) {
+            $this->interventions->removeElement($intervention);
+            // set the owning side to null (unless already changed)
+            if ($intervention->getClient() === $this) {
+                $intervention->setClient(null);
+            }
+        }
 
         return $this;
     }
