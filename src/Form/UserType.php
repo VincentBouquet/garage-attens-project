c1a0652e2bb507e2ec9bf871a5e07d77323f6d72
Sandra <oawx72@gmail.com>
2020-10-15 11:24:21 +0200

    ajouter et modifier un salarié OK mais pas de modification de mot de passe pour le moment

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -12,7 +12,6 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('password')
             ->add('firstname')
             ->add('lastname')
             ->add('phone')
