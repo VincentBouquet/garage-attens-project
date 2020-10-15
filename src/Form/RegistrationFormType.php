709863b52592d6103e18393c43ba4275b59ad437
Vinent Bouquet <bouquet.vincent704@orange.fr>
2020-10-15 12:29:22 +0200

    Suppression de l'id dans register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,7 +17,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id')
+            /*->add('id')*/
             ->add('firstname')
             ->add('lastname')
             ->add('phone')
